168e12246d19d0b065209cd2cd5789f51d592c22
philip <devnull@localhost>
2017-04-04 10:43:35 -0400

    First (long overdue) commit of  water_quality.py and util.py. They are complete and correct, AFAICT. Will next write siblings of water_quality.py and then util.py might change.

---
+++ util.py
@@ -0,0 +1,294 @@
+import os
+import time
+import urllib.request
+import urllib.parse
+import urllib.error
+import datetime
+import time
+import json
+import http.client
+
+HOST = 'data.chesapeakebay.net'
+
+# The host accepts api.json and api.JSON in the URLs. Our convention is to always use lower case.
+BASE_URL = '/api.json'
+
+# POLITENESS_DELAY controls how long we'll wait between requests to be nice to the
+# destination server. This data is organized into lots and lots of small files, and each file
+# download pauses for the POLITENESS_DELAY, so adjusting this just a little can have a big
+# effect on how long it takes to complete a set of downloads.
+# As of March 2017, http://datahub.chesapeakebay.net/robots.txt doesn't exist, so the site itself
+# gives us no guidance on what a polite crawl delay might be.
+POLITENESS_DELAY = 0.5
+
+# Connection attempt timeout, in seconds.
+TIMEOUT = 60.0
+
+# N_CONNECTION_RETRIES expresses the number of attempts at connecting after a timeout before
+# giving up entirely.
+N_CONNECTION_RETRIES = 3
+
+# N_DOWNLOAD_RETRIES expresses the number of attempts at downloading a file despite errors
+# (e.g. connection reset) before giving up entirely.
+N_DOWNLOAD_RETRIES = 3
+
+# I love ISO format, but when your API specifies M/D/YYYY format, that's what you gotta use.
+HISTORICAL_START_DATE_M_D_YYYY = '7-2-1949'
+HISTORICAL_END_DATE_M_D_YYYY = '8-13-1982'
+HISTORICAL_START_DATE_ISO = datetime.datetime.strptime(HISTORICAL_START_DATE_M_D_YYYY, '%m-%d-%Y').date().isoformat()
+HISTORICAL_END_DATE_ISO = datetime.datetime.strptime(HISTORICAL_END_DATE_M_D_YYYY, '%m-%d-%Y').date().isoformat()
+
+# "Present" water quality data begins 16 Jan 1984. This is hardcoded in WaterQuality.js (line 232)
+PRESENT_START_DATE_M_D_YYYY = '1-16-1984'
+PRESENT_END_DATE_M_D_YYYY = '3-31-2017'
+PRESENT_START_DATE_ISO = datetime.datetime.strptime(PRESENT_START_DATE_M_D_YYYY, '%m-%d-%Y').date().isoformat()
+PRESENT_END_DATE_ISO = datetime.datetime.strptime(PRESENT_END_DATE_M_D_YYYY, '%m-%d-%Y').date().isoformat()
+
+
+# GEOGRAPHICAL_TYPE_ATTRIBUTE_ID_MAP maps the 6 geographical types to the text strings of the IDs
+# used to represent them in the JSON. This same map is hardcoded in the function
+# _getGeographicalIDList() in WaterQuality.js (line 459) so I feel pretty safe hardcoding it here.
+GEOGRAPHICAL_TYPE_ATTRIBUTE_ID_MAP = {'HUC8': 'HUCEightId',
+                                      'HUC12': 'HUCTwelveId',
+                                      'FIPS': 'FIPSStateCountyCodeId',
+                                      'CBSeg2003': 'CBSegment2003Id',
+                                      'SegmentShed2009': 'CBSegmentShed2009Id',
+                                      'Station': 'MonitoringLocationId',
+                                      }
+
+# _http_connection is a global for use by this module only. Making it global allows us to keep
+# the HTTP connection alive between requests which is more polite to the remote server.
+_http_connection = None
+
+def _connect_to_server():
+    """Internal-use only function for establishing an HTTP connection to chesapeakebay.net"""
+    connection = None
+
+    # Connection timeouts are not frequent, but they happen often enough and it's a real
+    # disappointment to find that the script you thought had been running for hours actually
+    # died on a connection timeout. For that reason, I added some retries on connection
+    # timeouts.
+    n_retries = N_CONNECTION_RETRIES
+    connected = False
+    while not connected and n_retries:
+        # Take a quick nap to give the remote server a moment to pull itself together.
+        time.sleep(1)
+        try:
+            connection = http.client.HTTPConnection(HOST)
+        except urllib.error.URLError as error:
+            if isinstance(error.reason, TimeoutError) and n_retries:
+                msg = 'Connection timed out after {} seconds; will retry {} times.'
+                print(msg.format(TIMEOUT, n_retries))
+                n_retries -= 1
+            else:
+                # Print timestamp so that I know when this died. It's useful when I'm
+                # running this overnight.
+                print(datetime.datetime.now().isoformat())
+                raise
+        else:
+            connected = True
+
+    return connection
+
+
+def url_to_filename(url):
+    """Given a URL to the chesapeakebay.net server, returns a corresponding filename. The URL can
+    be whole or partial. The filename will always be in the data directory and is relative to
+    the tools directory (i.e. the filename always starts with '../data/').
+
+    This function strips the invariant parts from the start of the URL. It also translates dates
+    in the URL into ISO format for improved parseability and legibility.
+
+    Examples:
+    https://data.chesapeakebay.net/api.JSON/WaterQuality/WaterQuality/1-16-1984/3-31-2017/2/12/HUC8/4/30 ==>
+    ../data/WaterQuality/WaterQuality/1984-01-16_to_2017-03-31/2/12/HUC8/4/30.json
+
+    http://data.chesapeakebay.net/api.JSON/WaterQuality/WaterQuality/1-16-1984/3-31-2017/2/12/HUC8/4/30 ==>
+    ../data/WaterQuality/WaterQuality/1984-01-16_to_2017-03-31/2/12/HUC8/4/30.json
+
+    """
+    url = url.replace("api.JSON", "api.json")
+
+    if url.startswith('http://'):
+        url = url[7:]
+    if url.startswith('https://'):
+        url = url[8:]
+    if url.startswith(HOST):
+        url = url[len(HOST):]
+    if url.startswith(BASE_URL):
+        url = url[len(BASE_URL):]
+    if url.startswith('/'):
+        url = url[1:]
+
+    date_range = HISTORICAL_START_DATE_M_D_YYYY + '/' + HISTORICAL_END_DATE_M_D_YYYY
+    if date_range in url:
+        start_date = datetime.datetime.strptime(HISTORICAL_START_DATE_M_D_YYYY, '%m-%d-%Y').date()
+        end_date = datetime.datetime.strptime(HISTORICAL_END_DATE_M_D_YYYY, '%m-%d-%Y').date()
+        url = url.replace(date_range, start_date.isoformat() + '_to_' + end_date.isoformat())
+
+    date_range = PRESENT_START_DATE_M_D_YYYY + '/' + PRESENT_END_DATE_M_D_YYYY
+    if date_range in url:
+        url = url.replace(date_range, PRESENT_START_DATE_ISO + '_to_' + PRESENT_END_DATE_ISO)
+
+    url += '.json'
+
+    return os.path.join('..', 'data', url)
+
+
+def extract_attribute_ids(geographical_type_id, attributes):
+    """Given a list of dicts containing attribute ids, return a list of those IDs.
+
+    The attributes come from a file like this one:
+    https://data.chesapeakebay.net/api.json/WaterQuality/Station/HUC8/
+    Same as:
+    ../data/WaterQuality/Station/HUC8.json
+
+    Here's a sample --
+    [
+        {
+            "HUC8": "02050101",
+            "HUC8Description": "Upper Susquehanna",
+            "HUCEightId": 2
+        },
+        {
+            "HUC8": "02050102",
+            "HUC8Description": "Chenango",
+            "HUCEightId": 3
+        },
+        {
+            "HUC8": "02050103",
+            "HUC8Description": "Owego-Wappasening",
+            "HUCEightId": 4
+        }
+    ]
+
+    For the data above, this function would return the HUCEightIds [2, 3, 4].
+    """
+    id_name = GEOGRAPHICAL_TYPE_ATTRIBUTE_ID_MAP[geographical_type_id]
+
+    return [attribute[id_name] for attribute in attributes]
+
+
+def create_filename_directories(filename):
+    """Given a filename, ensures the directories expressed in the filename exist, creating them
+    if necessary.
+    """
+    # Create target directories if necessary.
+    path = os.path.dirname(filename)
+    # makedirs "will become confused" if '..' is present, so in case it is, normalize path.
+    # ref: Python doc
+    path = os.path.normpath(path)
+    os.makedirs(path, exist_ok=True)
+
+
+def post_attribute_ids(namespace, specifier, attribute_ids):
+    """POSTs a list of attribute IDs to the URL specified and returns the JSON response."""
+    url_path = BASE_URL + '/' + namespace + '/' + specifier
+
+    filename = url_to_filename(url_path)
+
+    if os.path.exists(filename):
+        print('{} already exists; skipping download.'.format(filename))
+        with open(filename, 'rb') as f:
+            data = json.load(f)
+    else:
+        print('POSTing to {}...'.format(url_path))
+
+        data = {'parametersList': ','.join([str(attribute_id) for attribute_id in attribute_ids])}
+        data = urllib.parse.urlencode(data)
+        data = data.encode('utf-8')
+
+        with urllib.request.urlopen('http://' + HOST + url_path, data) as f:
+            data = f.read()
+
+        print('Writing {}...'.format(filename))
+        # Deserialize to accomplish 2 things -- first, tests that what I got is valid JSON.
+        # Second, so that I can write it to disk nicely formatted (which helps with debugging).
+        data = json.loads(data)
+        create_filename_directories(filename)
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent='\t')
+
+    return data
+
+
+def download_and_jsonify(namespace, specifier):
+    """Same as download(), but also JSONifies and returns the file contents.
+    """
+    filename = download(namespace, specifier)
+    with open(filename, 'r') as f:
+        the_json_data = json.load(f)
+    return the_json_data
+
+
+def download(namespace, specifier):
+    """Given a namespace (e.g. WaterQuality) and a partial URL (e.g. Station/HUC8/3), checks to
+    see that the associated file exists, and if it does not, downloads it.
+    """
+    global _http_connection
+
+    if not _http_connection:
+        print("Reconnecting...")
+        _http_connection = _connect_to_server()
+
+    url = namespace + '/' + specifier
+
+    filename = url_to_filename(url)
+
+    if os.path.exists(filename):
+        print('{} already exists; skipping.'.format(filename))
+    else:
+        time.sleep(POLITENESS_DELAY)
+
+        url = BASE_URL + '/' + urllib.parse.quote(url)
+        print('Downloading {}...'.format(url))
+        data_received = False
+        n_retries = N_DOWNLOAD_RETRIES
+        while not data_received and n_retries:
+            if not _http_connection:
+                _http_connection = _connect_to_server()
+
+            try:
+                _http_connection.request('GET', url)
+            except http.client.NotConnected:
+                # Connection was dropped
+                if n_retries:
+                    _http_connection = None
+                    print('Connection was dropped; will retry {} times.'.format(n_retries))
+                    n_retries -= 1
+                else:
+                    # Print timestamp so that I know when this died. It's useful when I'm
+                    # running this overnight.
+                    print(datetime.datetime.now().isoformat())
+                    raise
+
+            if _http_connection:
+                try:
+                    response = _http_connection.getresponse()
+                except ConnectionResetError:
+                    if n_retries:
+                        _http_connection = None
+                        print('Connection was reset; will retry {} times.'.format(n_retries))
+                        n_retries -= 1
+                    else:
+                        # Print timestamp so that I know when this died. It's useful when I'm
+                        # running this overnight.
+                        print(datetime.datetime.now().isoformat())
+                        raise
+                else:
+                    if response.status == 200:
+                        data = response.read()
+                        data_received = True
+                    else:
+                        raise ValueError("response.status == {}".format(response.status))
+
+        create_filename_directories(filename)
+
+        print('Writing {}...'.format(filename))
+        # Deserialize to accomplish 2 things -- first, tests that what I got is valid JSON.
+        # Second, so that I can write it to disk nicely formatted (which helps with debugging).
+        data = json.loads(data)
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent='\t')
+
+    return filename
---
+++ water_quality.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+
+# Python imports
+import os
+import json
+import collections
+import pprint
+pp=pprint.pprint
+
+# Project imports
+import util
+
+NAMESPACE = 'WaterQuality'
+
+"""
+WaterQuality data has 2 top-level categories -- CBI (historical) and present data. CBI data runs
+from 1949 - 1982. Present data runs from 1982 to present.
+
+CBI data is relatively simple compared to present data.
+"""
+
+# Fetch & read geographical attrs (e.g. HUC8, FIPS, etc.)
+# http://data.chesapeakebay.net/api.json/WaterQuality/CbiGeographicalAttributes
+geographical_types = util.download_and_jsonify(NAMESPACE, 'CbiGeographicalAttributes')
+geographical_type_ids = [d['GeoTypeId'] for d in geographical_types]
+
+for geographical_type_id in geographical_type_ids:
+    # Use this URL to get attributes (station IDs)
+    # http://data.chesapeakebay.net/api.json/WaterQuality/CBI/7-2-1949/8-13-1982/HUC12
+    # Sample (abbreviated) response --
+    #     [
+    #         {
+    #             "AvailableRecords": 16035,
+    #             "HUC12": "020403040502",
+    #             "HUC12Description": "020403040502-Atlantic Ocean",
+    #             "HUCTwelveId": 20
+    #         },
+    #         {
+    #             "AvailableRecords": 66,
+    #             "HUC12": "020503061713",
+    #             "HUC12Description": "Rock Run-Susquehanna River",
+    #             "HUCTwelveId": 767
+    #         },
+    #         {
+    #             "AvailableRecords": 171142,
+    #             "HUC12": "020600010000",
+    #             "HUC12Description": "Upper Chesapeake Bay",
+    #             "HUCTwelveId": 768
+    #         },
+    #         {
+    #             "AvailableRecords": 14,
+    #             "HUC12": "020600020105",
+    #             "HUC12Description": "Elk Neck-Upper Chesapeake Bay",
+    #             "HUCTwelveId": 773
+    #         }
+    #     ]
+    #
+    path = 'CBI/{}/{}/{}'.format(util.HISTORICAL_START_DATE_M_D_YYYY,
+                                 util.HISTORICAL_END_DATE_M_D_YYYY,
+                                 geographical_type_id)
+
+    attributes = util.download_and_jsonify(NAMESPACE, path)
+
+    attribute_ids = util.extract_attribute_ids(geographical_type_id, attributes)
+
+    for attribute_id in attribute_ids:
+        path = 'CBI/{}/{}/{}/{}'.format(util.HISTORICAL_START_DATE_M_D_YYYY,
+                                        util.HISTORICAL_END_DATE_M_D_YYYY,
+                                        geographical_type_id,
+                                        attribute_id)
+        # e.g. https://data.chesapeakebay.net/api.JSON/WaterQuality/CBI/7-2-1949/8-13-1982/HUC12/20
+        util.download(NAMESPACE, path)
+
+# OK, done with historical data.
+# Modern water quality data has 5 data types: Station Information, Monitoring Event Data,
+# Water Quality Data, Light Attenuation Data, and Optical Density Data. Station info is
+# significantly different from the rest, so it gets handled in its own loop.
+
+# Each of the 5 data types requires some study to understand and download properly. I doubt
+# they'll add new data types, but if they do, this code will break. That being the case, I
+# check here to ensure that the data types are what I expect them to be, and fail loudly if not.
+data_types = util.download_and_jsonify(NAMESPACE, 'DataTypes')
+data_types = sorted([d['DataTypeId'] for d in data_types])
+expected_data_types = sorted(('Station', 'MonitorEvent', 'WaterQuality', 'LightAttenuation',
+                              'OpticalDensity'))
+if data_types != expected_data_types:
+    print('Received data types: ' + ', '.join(data_types))
+    print('Expected data types: ' + ', '.join(expected_data_types))
+    raise ValueError('Unexpected or missing data type(s)')
+
+# Fetch & read geographical types (e.g. HUC8, FIPS, etc.)
+geographical_types = util.download_and_jsonify(NAMESPACE, 'GeographicalAttributes')
+geographical_type_ids = [d['GeoTypeId'] for d in geographical_types]
+
+# Handle WaterQuality/Station which is a bit different from its sibling data types.
+for geographical_type_id in geographical_type_ids:
+    path = 'Station/{}'.format(geographical_type_id)
+    attributes = util.download_and_jsonify(NAMESPACE, path)
+
+    attribute_ids = util.extract_attribute_ids(geographical_type_id, attributes)
+
+    for attribute_id in attribute_ids:
+        path = 'Station/{}/{}'.format(geographical_type_id, attribute_id)
+        # e.g. https://data.chesapeakebay.net/api.JSON/WaterQuality/Station/HUC8/2
+        util.download(NAMESPACE, path)
+
+# Remove Station from the list of data types since it was just handled above.
+data_types = [data_type for data_type in data_types if data_type != 'Station']
+# All of the other data types (MonitorEvent, WaterQuality, LightAttenuation, and OpticalDensity)
+# behave mostly the same (although WayerQuality is a little more complex) and are handled by the
+# code below.
+programs = util.download_and_jsonify(NAMESPACE, 'Programs')
+projects_by_program = util.download_and_jsonify(NAMESPACE, 'ProjectsList')
+
+for data_type in data_types:
+    print('Starting data type "{}"...'.format(data_type))
+    for program in programs:
+        program_id = program['ProgramId']
+        projects = projects_by_program[str(program_id)]
+        project_ids = [project['ProjectId'] for project in projects]
+        for project_id in project_ids:
+            for geographical_type_id in geographical_type_ids:
+                # geographical_type_id is something like HUC8, FIPS, etc.
+
+                path = '{}/{}/{}/{}/{}/{}'.format(data_type,
+                                                  util.PRESENT_START_DATE_M_D_YYYY,
+                                                  util.PRESENT_END_DATE_M_D_YYYY,
+                                                  program_id,
+                                                  project_id,
+                                                  geographical_type_id)
+                # https://data.chesapeakebay.net/api.json/WaterQuality/OpticalDensity/3-29-2012/3-29-2017/2/12/HUC8/
+                attributes = util.download_and_jsonify(NAMESPACE, path)
+
+                attribute_ids = util.extract_attribute_ids(geographical_type_id, attributes)
+
+                if data_type == 'WaterQuality':
+                    # The WaterQuality data type uses one more level of description than its
+                    # sibling data types. That level is called the 'parameter' and it describes
+                    # what element of water quality one is interested in. Examples include
+                    # 'Whole 5-Day Biochemical Oxygen Demand' (BOD5W) or 'Dissolved Inorganic
+                    # Nitrogen' (DIN).
+                    # For reasons I don't understand, the list of valid params for a given
+                    # geographical type and list of stations is fetched from the remote server
+                    # as the response to a POST. Maybe because constructing a URL containing
+                    # all the attribute ids would be too long in some cases?
+
+                    # From line 178 of WaterQuality.js
+                    parameters = util.post_attribute_ids(NAMESPACE, path + '/Parameters',
+                                                         attribute_ids)
+
+                    # Parameters is a list of dicts; here's an example of one --
+                    #    {'HUCEightId': 20,
+                    #      'ProgramId': 2,
+                    #      'ProjectId': 12,
+                    #      'SubstanceIdentificationDescription': 'Whole 5-Day Biochemical Oxygen Demand',
+                    #      'SubstanceIdentificationId': 12,
+                    #      'SubstanceIdentificationName': 'BOD5W'}
+
+                    # ProgramId and ProjectId are the same for every entry.
+
+                    # I want to create a dict that maps geo type ids to a list of the valid
+                    # parameter ids (i.e. SubstanceIdentificationId) for that geo type. The
+                    # function _createParameterList() in WaterQuality.js does more or less the
+                    # same thing.
+                    key_name = util.GEOGRAPHICAL_TYPE_ATTRIBUTE_ID_MAP[geographical_type_id]
+                    # key_name is something like 'HUCEightId'
+                    parameters_temp = collections.defaultdict(list)
+                    for parameter in parameters:
+                        parameter_id = parameter['SubstanceIdentificationId']
+                        parameters_temp[parameter[key_name]].append(parameter_id)
+
+                    parameters = parameters_temp
+                else:
+                    parameters = {}
+
+                for attribute_id in attribute_ids:
+                    path = (data_type,
+                            util.PRESENT_START_DATE_M_D_YYYY,
+                            util.PRESENT_END_DATE_M_D_YYYY,
+                            program_id,
+                            project_id,
+                            geographical_type_id,
+                            attribute_id)
+                    path = '/'.join([str(element) for element in path])
+
+                    if data_type == 'WaterQuality':
+                        # WaterQuality uses an extra param as described above.
+                        parameter_ids = parameters[attribute_id]
+
+                        for parameter_id in parameter_ids:
+                            # e.g. https://data.chesapeakebay.net/api.JSON/WaterQuality/OpticalDensity/3-29-2012/3-29-2017/2/12/HUC8/20/119
+                            util.download(NAMESPACE, path + '/' + str(parameter_id))
+                    else:
+                        # e.g. https://data.chesapeakebay.net/api.JSON/WaterQuality/OpticalDensity/3-29-2012/3-29-2017/2/12/HUC8/20
+                        util.download(NAMESPACE, path)
