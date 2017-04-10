fcd9f8d3cc79c5178a2be060949428db0dc9233a
philip <devnull@localhost>
2017-04-10 17:43:04 -0400

    First working draft of fluorescence

--- fluorescence.py
+++ fluorescence.py
@@ -12,162 +12,58 @@ import util
 
 NAMESPACE = 'Fluorescence'
 
+
 """
-LivingResources data has 3 top-level categories (programs) -- Tidal Plankton Data, Tidal Benthic
-Data, and Nontidal Benthic Data.
+Fluorescence's top level category is called Data Types, and as of this writing there's only two
+entries: Horizontal and Vertical Fluorescence Data.
+
+Beneath that, there's a subdivision of Geographical attributes (HUC8, FIPS, etc.).
+
+Those two categories and a date range cover everything in Fluorescence.
 """
 
-# # https://data.chesapeakebay.net/api.json/LivingResources/Programs
-# programs = util.download_and_jsonify(NAMESPACE, 'Programs')
-# program_ids = sorted([d['ProgramId'] for d in programs])
-# # [{
-# #     "ProgramId": "TidalPlankton",
-# #     "ProgramIdentifier": "TPD",
-# #     "ProgramName": "Tidal Plankton Data"
-# # }, {
-# #     "ProgramId": "TidalBenthic",
-# #     "ProgramIdentifier": "TBD",
-# #     "ProgramName": "Tidal Benthic Data"
-# # }, {
-# #     "ProgramId": "NontidalBenthic",
-# #     "ProgramIdentifier": "NBD",
-# #     "ProgramName": "Nontidal Benthic Data"
-# # }]
+# https://data.chesapeakebay.net/api.json/Fluorescence/DataTypes
+data_types = util.download_and_jsonify(NAMESPACE, 'DataTypes')
+data_type_ids = [d['DataTypeId'] for d in data_types]
 
-# # Each of the 3 programs requires some study to understand and download properly. I doubt
-# # they'll add/delete/change programs, but if they do, this code might break. That being the case, I
-# # check here to ensure that the programs are what I expect them to be, and fail loudly if not.
-# expected_program_ids = sorted(('TidalPlankton', 'TidalBenthic', 'NontidalBenthic'))
-# if program_ids != expected_program_ids:
-#     print('Received programs: ' + ', '.join(program_ids))
-#     print('Expected programs: ' + ', '.join(expected_program_ids))
-#     raise ValueError('Unexpected or missing program(s)')
+# https://data.chesapeakebay.net/api.json/Fluorescence/GeographicalAttributes
+geographical_types = util.download_and_jsonify(NAMESPACE, 'GeographicalAttributes')
+geographical_type_ids = [d['GeoTypeId'] for d in geographical_types]
 
-# # Each of the 3 programs has a number of different data types. They all behave the same, except
-# # for /TidalPlankton/Station which requires special handling. I handle it here.
+for data_type_id in data_type_ids:
+    for geographical_type_id in geographical_type_ids:
+        # e.g. https://data.chesapeakebay.net/api.json/Fluorescence/Vertical/Thu%20Aug%2002%201984/Mon%20Apr%2010%202017/HUC8/
+        path = '/'.join((data_type_id,
+                         util.FLUORESCENCE_START_DATE,
+                         util.FLUORESCENCE_END_DATE,
+                         geographical_type_id))
+        attributes = util.download_and_jsonify(NAMESPACE, path)
+        # The attributes in this file look very similar to the same files you find in Water Quality
+        # and Living Resources. They're used differently, however. Instead of using the
+        # attribute IDs defined in util.GEOGRAPHICAL_TYPE_ATTRIBUTE_ID_MAP, fluorescence uses
+        # the value of the geographical_type_id.
+        # For instance, using this sample data --
+        #     [{
+        #         "HUCEightId": 76,
+        #         "HUC8": "02050306  ",
+        #         "HUC8Description": "LOWER SUSQUEHANNA"
+        #     }, {
+        #         "HUCEightId": 77,
+        #         "HUC8": "02060001  ",
+        #         "HUC8Description": "UPPER CHESAPEAKE BAY"
+        #     }, {
+        #         "HUCEightId": 78,
+        #         "HUC8": "02060002  ",
+        #         "HUC8Description": "CHESTER-SASSAFRAS"
+        #     }]
+        # We want to extract the ids ('02050306', '02060001', '02060002').
+        attribute_ids = [d[geographical_type_id].strip() for d in attributes]
 
-# # https://data.chesapeakebay.net/api.json/LivingResources/TidalPlankton/GeographicalAttributes
-# geographical_types = util.download_and_jsonify(NAMESPACE, 'TidalPlankton/GeographicalAttributes')
-# # The last two elements of the DataTypes URL seem backwards to me but that's what they are.
-# # https://data.chesapeakebay.net/api.json/LivingResources/DataTypes/TidalPlankton
-# data_types = util.download_and_jsonify(NAMESPACE, 'DataTypes/TidalPlankton')
-# data_type_ids = [d['DataTypeId'] for d in data_types]
-
-# # For TidalPlankton, geographical_types is a dicts of lists keyed by data type. Simplify.
-# # ref: LivingResources.js (line 67)
-# temp_geographical_types = collections.defaultdict(list)
-# for data_type_id in data_type_ids:
-#     temp_geographical_types[data_type_id] = [d['GeoTypeId'] for
-#                                              d in geographical_types[data_type_id]]
-# geographical_types = temp_geographical_types
-
-# # geographical_types is now a dict like this --
-# #     {'MonitorEvent': ['HUC8', 'HUC12', 'FIPS', 'CBSeg2003', 'Station'],
-# #      'Reported': ['HUC8', 'HUC12', 'FIPS', 'CBSeg2003', 'Station'],
-# #      'Station': ['HUC8', 'HUC12', 'FIPS', 'CBSeg2003', 'SegmentShed2009', 'Station']}
-
-# # First handle program == TidalPlankton, geo type == Station
-# for geographical_type_id in geographical_types['Station']:
-#     if geographical_type_id != 'FIPS':
-#         path = '/'.join(('TidalPlankton', 'Station', geographical_type_id))
-#         # e.g. https://data.chesapeakebay.net/api.json/LivingResources/TidalPlankton/Station/HUC8/
-#         attributes = util.download_and_jsonify(NAMESPACE, path)
-#         attribute_ids = util.extract_attribute_ids(geographical_type_id, attributes)
-#         attribute_ids = map(str, attribute_ids)
-
-#         for attribute_id in attribute_ids:
-#             # e.g. https://data.chesapeakebay.net/api.JSON/LivingResources/TidalPlankton/Station/HUC8/20
-#             util.download(NAMESPACE, path + '/' + attribute_id)
-
-# # Remove Station since it was handled above
-# data_type_ids = [data_type_id for data_type_id in data_type_ids if data_type_id != 'Station']
-
-# # https://data.chesapeakebay.net/api.json/LivingResources/Projects/TidalPlankton
-# projects = util.download_and_jsonify(NAMESPACE, 'Projects/TidalPlankton')
-# project_ids = [str(d['ProjectId']) for d in projects]
-
-# # At this point, data_type_ids is probably just ('MonitorEvent', 'Reported')
-# for data_type_id in data_type_ids:
-#     for project_id in project_ids:
-#         for geographical_type_id in geographical_types[data_type_id]:
-#             path = '/'.join(('TidalPlankton',
-#                              data_type_id,
-#                              util.PRESENT_START_DATE_M_D_YYYY,
-#                              util.PRESENT_END_DATE_M_D_YYYY,
-#                              project_id,
-#                              geographical_type_id))
-
-#             # e.g. https://data.chesapeakebay.net/api.JSON/LivingResources/TidalPlankton/MonitorEvent/1-16-1984/4-4-2017/9/HUC12
-#             attributes = util.download_and_jsonify(NAMESPACE, path)
-#             attribute_ids = util.extract_attribute_ids(geographical_type_id, attributes)
-#             attribute_ids = map(str, attribute_ids)
-
-#             for attribute_id in attribute_ids:
-#                 path = '/'.join(('TidalPlankton',
-#                                  data_type_id,
-#                                  util.PRESENT_START_DATE_M_D_YYYY,
-#                                  util.PRESENT_END_DATE_M_D_YYYY,
-#                                  project_id,
-#                                  geographical_type_id,
-#                                  attribute_id))
-#                 # e.g. https://data.chesapeakebay.net/api.JSON/LivingResources/TidalPlankton/MonitorEvent/1-16-1984/4-4-2017/9/HUC12/767
-#                 util.download(NAMESPACE, path)
-
-# # OK, that takes care of all 3 TidalPlankton data types. Remove TidalPlankton since it's been
-# # handled.
-# program_ids = [program_id for program_id in program_ids if program_id != 'TidalPlankton']
-# # And now for the remaining programs. At this point, program_ids is probably just this --
-# #    ('NontidalBenthic', 'TidalBenthic')
-# for program_id in program_ids:
-#     print('Starting program {}...'.format(program_id))
-#     # e.g. https://data.chesapeakebay.net/api.json/LivingResources/DataTypes/TidalBenthic
-#     data_types = util.download_and_jsonify(NAMESPACE, 'DataTypes/' + program_id)
-#     data_type_ids = [d['DataTypeId'] for d in data_types]
-
-#     if program_id == 'TidalBenthic':
-#         import pprint; pp=pprint.pprint
-#         import pdb; pdb.set_trace()
-
-#         data_type_ids = [data_type_id for data_type_id in data_type_ids if data_type_id !='MonitorEvent']
-#         data_type_ids = [data_type_id for data_type_id in data_type_ids if data_type_id !='Biomass']
-#         data_type_ids = [data_type_id for data_type_id in data_type_ids if data_type_id !='Taxonomic']
-
-
-#     path = '/'.join((program_id, 'GeographicalAttributes'))
-#     # e.g. https://data.chesapeakebay.net/api.json/LivingResources/NontidalBenthic/GeographicalAttributes
-#     geographical_types = util.download_and_jsonify(NAMESPACE, path)
-#     geographical_type_ids = [d['GeoTypeId'] for d in geographical_types]
-
-#     for data_type_id in data_type_ids:
-#         print('Starting program {}, data type {}...'.format(program_id, data_type_id))
-#         # e.g. https://data.chesapeakebay.net/api.json/LivingResources/Projects/TidalBenthic
-#         projects = util.download_and_jsonify(NAMESPACE, 'Projects/' + program_id)
-#         project_ids = [str(d['ProjectId']) for d in projects]
-#         for project_id in project_ids:
-#             params = (program_id, data_type_id, project_id)
-#             print('Starting program {}, data type {}, project {}...'.format(*params))
-#             for geographical_type_id in geographical_type_ids:
-#                 params = (program_id, data_type_id, project_id, geographical_type_id)
-#                 print('Starting program {}, data type {}, project {}, geo type {}...'.format(*params))
-#                 # geographical_type_id is something like HUC8, FIPS, etc.
-#                 path = '/'.join((program_id,
-#                                  data_type_id,
-#                                  util.PRESENT_START_DATE_M_D_YYYY,
-#                                  util.PRESENT_END_DATE_M_D_YYYY,
-#                                  project_id,
-#                                  geographical_type_id))
-#                 # https://data.chesapeakebay.net/api.JSON/LivingResources/TidalPlankton/MonitorEvent/4-4-2012/4-4-2017/17/HUC12
-#                 attributes = util.download_and_jsonify(NAMESPACE, path)
-#                 attribute_ids = util.extract_attribute_ids(geographical_type_id, attributes)
-#                 attribute_ids = map(str, attribute_ids)
-
-#                 for attribute_id in attribute_ids:
-#                     path = '/'.join((program_id,
-#                                      data_type_id,
-#                                      util.PRESENT_START_DATE_M_D_YYYY,
-#                                      util.PRESENT_END_DATE_M_D_YYYY,
-#                                      project_id,
-#                                      geographical_type_id,
-#                                      attribute_id))
-#                     # e.g. https://data.chesapeakebay.net/api.JSON/LivingResources/TidalPlankton/MonitorEvent/4-4-2012/4-4-2017/17/HUC12/781
-#                     util.download(NAMESPACE, path)
+        for attribute_id in attribute_ids:
+            # e.g. https://data.chesapeakebay.net/api.JSON/Fluorescence/Vertical/8-2-1984/4-10-2017/HUC8/02060002
+            path = '/'.join((data_type_id,
+                             util.FLUORESCENCE_START_DATE,
+                             util.FLUORESCENCE_END_DATE,
+                             geographical_type_id,
+                             attribute_id))
+            util.download(NAMESPACE, path)
