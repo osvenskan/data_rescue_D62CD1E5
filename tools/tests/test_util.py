aa9aef65f1c5a9611129575eeaa182611bc85175
osvenskan <philip@pyspoken.com>
2017-12-13 19:26:24 -0500

    Fix tests broken when I changed END_DATE.

--- tools/tests/test_util.py
+++ tools/tests/test_util.py
@@ -1,6 +1,15 @@
 import unittest
+import datetime
 
 import util
+from util_date import END_DATE
+
+end_date = datetime.date(*END_DATE)
+
+END_DATE_MM_DD_YYYY = '{}-{}-{}'.format(end_date.month, end_date.day, end_date.year)
+END_DATE_ISO = end_date.isoformat()
+END_DATE_CTIME = end_date.ctime().replace('00:00:00 ', '')
+
 
 class TestUrlToFilename(unittest.TestCase):
     """Exercise util.url_to_filename()"""
@@ -36,12 +45,12 @@ class TestUrlToFilename(unittest.TestCase):
               '../data/WaterQuality/CBI/1949-07-02_to_1982-08-13/2/12/HUC8/4/30.json'),
 
              # Modern date range
-             ('/WaterQuality/WaterQuality/1-16-1984/3-31-2017/2/12/HUC8/4/30',
-              '../data/WaterQuality/WaterQuality/1984-01-16_to_2017-03-31/2/12/HUC8/4/30.json'),
+             ('/WaterQuality/WaterQuality/1-16-1984/{}/2/12/HUC8/4/30'.format(END_DATE_MM_DD_YYYY),
+              '../data/WaterQuality/WaterQuality/1984-01-16_to_{}/2/12/HUC8/4/30.json'.format(END_DATE_ISO)),
 
              # Fluorescence date range
-             ('/Fluorescence/Vertical/Thu Aug 02 1984/Fri Mar 31 2017/HUC8/02060002',
-              '../data/Fluorescence/Vertical/1984-08-02_to_2017-03-31/HUC8/02060002.json'),
+             ('/Fluorescence/Vertical/Thu Aug 02 1984/{}/HUC8/02060002'.format(END_DATE_CTIME),
+              '../data/Fluorescence/Vertical/1984-08-02_to_{}/HUC8/02060002.json'.format(END_DATE_ISO)),
 
              # Short URL
              ('/WaterQuality/DataTypes',
